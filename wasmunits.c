103ff25e363a584784aff44bae599af446e5b165
Yanosh Kunsh <me@yanosh.net>
2021-10-12 11:36:08 +0300

    add capabilites for handling unit definitions

--- wasmunits.c
+++ wasmunits.c
@@ -3,8 +3,16 @@
 
 EMSCRIPTEN_KEEPALIVE
 int convert_unit(char *youHave, char *youWant) {
-	int argc = 4;
-	char *argv[] = {"units", "--terse", youHave, youWant};
+	int argc = strlen(youWant) ? 4 : 3;
+	char *argv[argc];
+	
+	argv[0] = "units";
+	argv[1] = "--terse";
+	argv[2] = youHave;
+	
+	if (strlen(youWant)) {
+		argv[3] = youWant;
+	}
 	
 	return unitsHandler(argc, argv);
 }
